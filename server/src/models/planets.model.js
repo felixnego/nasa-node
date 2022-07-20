a46b9bab38d1ae46558d710de69c2d6452877d36
Felix Negoita <felixnegoita@Felixs-MacBook-Pro.local>
2022-07-20 16:37:46 +0300

    added in memory data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,1 +1,49 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
 export const PLANETS = [];
+
+/**
+ * @param {
+ * {
+ * koi_disposition:string,
+ * koi_insol:string,
+ * koi_prad: string
+ * }
+ * } planet
+ */
+function isHabitablePlanet(planet) {
+    return planet.koi_disposition === 'CONFIRMED'
+        && planet.koi_insol > 0.36 && planet.koi_insol < 1.11
+        && planet.koi_prad < 1.6;
+}
+
+export function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(parse({
+                comment: '#',
+                columns: true,
+            }))  //returns a stream as a result
+            .on('data', (data) => {
+                if (isHabitablePlanet(data)) {
+                    PLANETS.push(data);
+                }
+            })
+            .on('error', (error) => {
+                reject(error);
+            })
+            .on('end', () => {
+                console.log('Completed reading file. Results below:');
+                console.log(`${PLANETS.length} habitable planets found!`);
+                resolve();
+            });
+    })
+}
+
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,18 @@
 import express from "express";
-import {planetsRouter} from "./routes/planets/planets.router.js";
+import cors from "cors";
+import { planetsRouter } from "./routes/planets/planets.router.js";
+import { loadPlanetsData } from "./models/planets.model.js";
 
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(planetsRouter);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
+
+await loadPlanetsData();
 
 app.listen(PORT, () => {
     console.log(`Server started and listening on port: ${PORT}...`);
